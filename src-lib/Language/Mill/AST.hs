7833ec761e6a0da5edae2bb312547148412b8bef
Radek Slupik <rightfold@gmail.com>
2015-06-29 11:14:24 +0200

    Add foreign sub decl AST node type

--- src-lib/Language/Mill/AST.hs
+++ src-lib/Language/Mill/AST.hs
@@ -1,46 +1,34 @@
-module Language.Mill.AST
-( ModuleName(..)
-, Name(..)
+module Language.Mill.AST where
 
-, ParameterList(..)
-, Parameter(..)
-
-, Type(..)
-, Module(..)
-, Decl(..)
-, Expr(..)
-, Stmt(..)
-) where
-
-data ModuleName = ModuleName [String]
-  deriving (Eq, Show)
+newtype ModuleName = ModuleName [String] deriving (Eq, Show)
 
 data Name
     = UnqualifiedName String
     | QualifiedName ModuleName String
-  deriving (Eq, Show)
+    deriving (Eq, Show)
+
+data Type = NamedType Name deriving (Eq, Show)
 
 type ParameterList = [Parameter]
-data Parameter = Parameter String Type
-  deriving (Eq, Show)
+data Parameter = Parameter String Type deriving (Eq, Show)
 
-data Type = NamedType Name
-  deriving (Eq, Show)
-
-data Module = Module [Decl]
-  deriving (Eq, Show)
+data Module = Module [Decl] deriving (Eq, Show)
 
 data Decl
     = ImportDecl ModuleName
     | SubDecl String ParameterList Type Expr
-  deriving (Eq, Show)
+    | ForeignSubDecl ForeignSource CallingConvention String ParameterList Type
+    deriving (Eq, Show)
+
+newtype ForeignSource = ForeignSource String deriving (Eq, Show)
+newtype CallingConvention = CallingConvention Name deriving (Eq, Show)
 
 data Expr
     = BlockExpr [Stmt]
     | CallExpr Expr [Expr]
     | NameExpr Name
     | StringLiteralExpr String
-  deriving (Eq, Show)
+    deriving (Eq, Show)
 
 data Stmt
     = ExprStmt Expr
