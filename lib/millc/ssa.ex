404d384a493dfd4d465824fff2f2e3d67c2a1361
Radek Slupik <rightfold@gmail.com>
2015-07-26 12:09:30 +0200

    Implement dumping SSA to DOT

---
+++ lib/millc/ssa.ex
@@ -0,0 +1,102 @@
+defmodule Millc.SSA do
+  def outgoing(block) do
+    List.foldl(block, HashSet.new, fn({_instr_id, instr}, target_ids) ->
+      case instr do
+        {:goto, target_id} ->
+          Set.put(target_ids, target_id)
+
+        {:if, _condition, then_id, else_id} ->
+          target_ids |> Set.put(then_id) |> Set.put(else_id)
+
+        _ ->
+          target_ids
+      end
+    end)
+  end
+
+  def dump(graph) do
+    {:ok, builder} = Agent.start_link(fn() -> "" end)
+    try do
+      build = fn(str) ->
+        :ok = Agent.update(builder, fn(txt) -> txt <> str end)
+      end
+
+      build.("digraph {\n")
+
+      Enum.each(graph, fn({block_id, block}) ->
+        dump_block(build, block_id, block)
+      end)
+
+      Enum.each(graph, fn({block_id, block}) ->
+        Enum.each(outgoing(block), fn(other_id) ->
+          build.("#{block_id} -> #{other_id}\n")
+        end)
+      end)
+
+      build.("}\n")
+
+      File.write(
+        "/Users/rightfold/Desktop/graph.dot",
+        Agent.get(builder, fn(txt) -> txt end),
+        [:write]
+      )
+    after
+      :ok = Agent.stop(builder)
+    end
+  end
+
+  defp dump_block(build, block_id, block) do
+    build.("""
+      #{block_id} [
+        shape = none
+        label = <<table border="0" cellborder="1" cellspacing="0">
+          <tr>
+            <td colspan="2">#{block_id}</td>
+          </tr>
+    """)
+    Enum.each(block, fn({instr_id, instr}) ->
+      build.("|")
+      dump_instr(build, instr_id, instr)
+    end)
+    build.("""
+        </table>>
+      ]\n
+    """)
+  end
+
+  defp dump_instr(build, instr_id, instr) do
+    build.("""
+      <tr>
+        <td align="right">#{instr_id}</td>
+        <td align="left">
+    """)
+
+    case instr do
+      {:call, callee, args} ->
+        build.("call #{callee} #{Enum.join(args, " ")}")
+
+      {:goto, block_id} ->
+        build.("goto #{block_id}")
+
+      {:if, condition, then_id, else_id} ->
+        build.("if #{condition} #{then_id} #{else_id}")
+
+      {:ldarg, index} ->
+        build.("ldarg #{index}")
+
+      {:ldint, value} ->
+        build.("ldint #{value}")
+
+      {:ldgbl, name} ->
+        build.("ldgbl #{Enum.join(name, ".")}")
+
+      {:ret, value} ->
+        build.("ret #{value}")
+    end
+
+    build.("""
+        </td>
+      </tr>\n
+    """)
+  end
+end
---
+++ test/millc/ssa_test.exs
@@ -0,0 +1,27 @@
+defmodule Millc.SSATest do
+  use ExUnit.Case
+
+  test "dump" do
+    graph = %{
+      "b1" => [
+        {"i1", {:ldgbl, ['mill', 'ord', '__gt__']}},
+        {"i2", {:ldarg, 0}},
+        {"i3", {:ldarg, 1}},
+        {"i4", {:goto, "b2"}},
+      ],
+      "b2" => [
+        {"i5", {:call, "i1", ["i2", "i3"]}},
+        {"i6", {:if, "i5", "b3", "b4"}},
+      ],
+      "b3" => [
+        {"i7", {:ldint, 1}},
+        {"i8", {:ret, "i7"}},
+      ],
+      "b4" => [
+        {"i9", {:ldint, 0}},
+        {"i10", {:ret, "i9"}},
+      ],
+    }
+    :ok = Millc.SSA.dump(graph)
+  end
+end
