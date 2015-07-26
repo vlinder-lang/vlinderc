34ad8a44507e38cc7bf7c4b2c2557b21bc91f92b
Radek Slupik <rightfold@gmail.com>
2015-07-26 14:43:58 +0200

    Resolve type decls

--- lib/millc/type.ex
+++ lib/millc/type.ex
@@ -1,4 +1,9 @@
 defmodule Millc.Type do
+  alias Millc.Name.ModuleSymbol, as: ModuleSymbol
+  alias Millc.Name.MemberSymbol, as: MemberSymbol
+  alias Millc.Name.LocalSymbol, as: LocalSymbol
+  alias Millc.Name.BuiltinSymbol, as: BuiltinSymbol
+
   defmodule StringType do
     defstruct []
   end
@@ -8,7 +13,7 @@ defmodule Millc.Type do
     defstruct element_types: nil
   end
 
-  defmodule SubroutineType do
+  defmodule SubType do
     @derive [Access]
     defstruct param_types: nil, return_type: nil
   end
@@ -59,5 +64,81 @@ defmodule Millc.Type do
 
       _ -> type
     end
+  end
+
+  defmodule Context do
+    @derive [Access]
+    defstruct decl_types: nil
+  end
+
+  def type_check(modules) do
+    ctx = %Context{
+      :decl_types => %{},
+    }
+
+    ctx = Dict.to_list(modules) |> List.foldl(ctx, fn({module_name, module}, ctx) ->
+      register_decl_types(ctx, module_name, module)
+    end)
+
+    {:ok, modules}
+  end
+
+  defp register_decl_types(ctx, module_name, {:module, decls, _meta}) do
+    List.foldl(decls, ctx, fn(decl, ctx) ->
+      register_decl_types(ctx, module_name, decl)
+    end)
+  end
+
+  defp register_decl_types(ctx, _module_name, {:import_decl, _, _meta}) do
+    ctx
+  end
+
+  defp register_decl_types(ctx, module_name, {:struct_decl, name, fields, _meta}) do
+    typed_fields = Enum.map(fields, fn({field_name, field_type_expr}) ->
+      {field_name, type_expr_to_type(field_type_expr)}
+    end)
+    decl_type = %StructDeclType{:name => name, :fields => typed_fields}
+    put_in(ctx, [:decl_types, {module_name, name}], decl_type)
+  end
+
+  defp register_decl_types(ctx, module_name, {:union_decl, name, constructors, _meta}) do
+    decl_type = %UnionDeclType{:name => name, :constructors => constructors}
+    put_in(ctx, [:decl_types, {module_name, name}], decl_type)
+  end
+
+  defp register_decl_types(ctx, module_name, {:alias_decl, name, aliases, _meta}) do
+    aliases_type = type_expr_to_type(aliases)
+    decl_type = %AliasDeclType{:name => name, :aliases => aliases_type}
+    put_in(ctx, [:decl_types, {module_name, name}], decl_type)
+  end
+
+  defp register_decl_types(ctx, _module_name, {:sub_decl, _, _, _, _, _meta}) do
+    ctx
+  end
+
+  defp type_expr_to_type({:name_type_expr, _name, meta}) do
+    case meta[:symbol] do
+      %ModuleSymbol{} ->
+        raise "this is a module, not a type"
+      %MemberSymbol{:module_name => module_name, :name => name} ->
+        %NamedType{:name => {module_name, name}}
+      %LocalSymbol{} ->
+        raise "local types are not yet supported"
+      %BuiltinSymbol{:name => name} ->
+        case name do
+          "__String" -> %StringType{}
+        end
+    end
+  end
+
+  defp type_expr_to_type({:sub_type_expr, param_type_exprs, return_type_expr, _meta}) do
+    param_types = Enum.map(param_type_exprs, &type_expr_to_type(&1))
+    return_type = type_expr_to_type(return_type_expr)
+    %SubType{:param_types => param_types, :return_type => return_type}
+  end
+
+  defp type_expr_to_type({:tuple_type_expr, element_type_exprs, _meta}) do
+    element_types = Enum.map(element_type_exprs, &type_expr_to_type(&1))
+    %TupleType{:element_types => element_types}
   end
 end
--- test/millc/type_test.exs
+++ test/millc/type_test.exs
@@ -2,7 +2,7 @@ defmodule Millc.TypeTest do
   import Millc.Type
   alias Millc.Type.StringType, as: StringType
   alias Millc.Type.TupleType, as: TupleType
-  alias Millc.Type.SubroutineType, as: SubroutineType
+  alias Millc.Type.SubType, as: SubType
   alias Millc.Type.NamedType, as: NamedType
 
   alias Millc.Type.UnionDeclType, as: UnionDeclType
@@ -13,27 +13,27 @@ defmodule Millc.TypeTest do
 
   test "same_type?" do
     decl_types = %{
-      ['mill', 'log', 'Level'] => %UnionDeclType{
-        :name => ['mill', 'log', 'Level'],
+      {["mill", "log"], "Level"} => %UnionDeclType{
+        :name => {["mill", "log"], "Level"},
         :constructors => [
-          {'Debug', []},
-          {'Info', []},
-          {'Warning', []},
-          {'Error', []},
-          {'Critical', []},
+          {"Debug", []},
+          {"Info", []},
+          {"Warning", []},
+          {"Error", []},
+          {"Critical", []},
         ],
       },
-      ['mill', 'log', 'Record'] => %StructDeclType{
-        :name => ['mill', 'log', 'Record'],
+      {["mill", "log"], "Record"} => %StructDeclType{
+        :name => {["mill", "log"], "Record"},
         :fields => [
-          {'level', %NamedType{:name => ['mill', 'log', 'Level']}},
-          {'message', %StringType{}},
+          {"level", %NamedType{:name => {["mill", "log"], "Level"}}},
+          {"message", %StringType{}},
         ],
       },
-      ['mill', 'log', 'Logger'] => %AliasDeclType{
-        :name => ['mill', 'log', 'Logger'],
-        :aliases => %SubroutineType{
-          :param_types => [%NamedType{:name => ['mill', 'log', 'Record']}],
+      {["mill", "log"], "Logger"} => %AliasDeclType{
+        :name => {["mill", "log"], "Logger"},
+        :aliases => %SubType{
+          :param_types => [%NamedType{:name => {["mill", "log"], "Record"}}],
           :return_type => %TupleType{:element_types => []},
         },
       },
@@ -41,21 +41,21 @@ defmodule Millc.TypeTest do
 
     [
       {
-        %NamedType{:name => ['mill', 'log', 'Level']},
-        %NamedType{:name => ['mill', 'log', 'Level']},
+        %NamedType{:name => {["mill", "log"], "Level"}},
+        %NamedType{:name => {["mill", "log"], "Level"}},
       },
       {
-        %NamedType{:name => ['mill', 'log', 'Record']},
-        %NamedType{:name => ['mill', 'log', 'Record']},
+        %NamedType{:name => {["mill", "log"], "Record"}},
+        %NamedType{:name => {["mill", "log"], "Record"}},
       },
       {
-        %NamedType{:name => ['mill', 'log', 'Logger']},
-        %NamedType{:name => ['mill', 'log', 'Logger']},
+        %NamedType{:name => {["mill", "log"], "Logger"}},
+        %NamedType{:name => {["mill", "log"], "Logger"}},
       },
       {
-        %NamedType{:name => ['mill', 'log', 'Logger']},
-        %SubroutineType{
-          :param_types => [%NamedType{:name => ['mill', 'log', 'Record']}],
+        %NamedType{:name => {["mill", "log"], "Logger"}},
+        %SubType{
+          :param_types => [%NamedType{:name => {["mill", "log"], "Record"}}],
           :return_type => %TupleType{:element_types => []},
         },
       },
@@ -63,5 +63,52 @@ defmodule Millc.TypeTest do
     |> Enum.each(fn({a, b}) ->
       assert(same_type?(decl_types, a, b))
     end)
+  end
+
+  test "type_check" do
+    mill_text_code = """
+      alias String = __String
+    """
+    {:ok, mill_text_tokens} = Millc.Lex.lex(mill_text_code)
+    {:ok, mill_text} = Millc.Parse.parse(mill_text_tokens)
+
+    mill_log_code = """
+      import mill.text
+
+      union Level {
+        Debug
+        Info
+        Warning
+        Error
+        Critical
+      }
+
+      struct Record {
+        level: Level
+        message: text.String
+      }
+
+      alias Logger = (Record) => ()
+
+      sub info(logger: Logger, message: text.String): () { }
+    """
+    {:ok, mill_log_tokens} = Millc.Lex.lex(mill_log_code)
+    {:ok, mill_log} = Millc.Parse.parse(mill_log_tokens)
+
+    modules = %{
+      ["mill", "text"] => mill_text,
+      ["mill", "log"] => mill_log,
+    }
+
+    {:ok, mill_text} = Millc.Name.resolve_module(["mill", "text"], modules)
+    {:ok, mill_log} = Millc.Name.resolve_module(["mill", "log"], modules)
+
+    modules = %{
+      ["mill", "text"] => mill_text,
+      ["mill", "log"] => mill_log,
+    }
+
+    {:ok, result} = type_check(modules)
+    IO.inspect(result)
   end
 end
