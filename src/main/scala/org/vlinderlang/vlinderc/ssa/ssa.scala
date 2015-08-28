1ac25fc8be4956acf7eeb6a5ac9e3ab7af69a366
rightfold <rightfold@gmail.com>
2015-08-28 23:14:21 +0200

    Add ssa.RetInst

--- src/main/scala/org/vlinderlang/vlinderc/ssa/ssa.scala
+++ src/main/scala/org/vlinderlang/vlinderc/ssa/ssa.scala
@@ -13,3 +13,4 @@ case class CallInst(callee: InstID, arguments: Vector[InstID]) extends Inst
 case class LdargInst(index: Int) extends Inst
 case class LdgblInst(module: ModuleName, name: String) extends Inst
 case class LdstrInst(value: String) extends Inst
+case class RetInst(value: InstID) extends Inst
