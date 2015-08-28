52cde000deacaf499df9575309673ad3de3dcbda
rightfold <rightfold@gmail.com>
2015-08-28 23:13:13 +0200

    Add SSA data structures needed by hello world program

---
+++ src/main/scala/org/vlinderlang/vlinderc/ssa/ssa.scala
@@ -0,0 +1,15 @@
+package org.vlinderlang.vlinderc.ssa
+
+import org.vlinderlang.vlinderc.ModuleName
+import scala.collection.immutable.ListMap
+
+class BlockID
+class InstID
+
+case class Block(insts: ListMap[InstID, Inst])
+
+sealed abstract class Inst
+case class CallInst(callee: InstID, arguments: Vector[InstID]) extends Inst
+case class LdargInst(index: Int) extends Inst
+case class LdgblInst(module: ModuleName, name: String) extends Inst
+case class LdstrInst(value: String) extends Inst
